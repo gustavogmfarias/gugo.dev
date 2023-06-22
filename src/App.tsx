fc5dabd913067084a3916753bf4bcd3b5653404a
Gustavo <gustavogmfarias@gmail.com>
2023-06-22 13:56:25 -0300

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  );
 }
