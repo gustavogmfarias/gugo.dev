2da43aade5c719a9dfb1516eb5add121dde053fc
Gustavo <gustavogmfarias@gmail.com>
2023-06-30 17:10:33 -0300

    fixing texts

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+let baseUrl = "";
+
+if (process.env.NODE_ENV === "production") {
+  // URL para ambiente de produção
+  baseUrl = "https://gugo-dev.vercel.app";
+} else {
+  // URL para ambiente de desenvolvimento (localhost)
+  baseUrl = "http://localhost:3333";
+}
+
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: baseUrl,
 });
