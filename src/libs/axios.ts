89f2b307651ef0e5e99aa2e79b46d99fe63ec07a
Gustavo <gustavogmfarias@gmail.com>
2023-06-30 17:22:13 -0300

    .

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -4,7 +4,7 @@ let baseUrl = "";
 
 if (process.env.NODE_ENV === "production") {
   // URL para ambiente de produção
-  baseUrl = "https://gugo-dev.vercel.app";
+  baseUrl = "https://gugo-dev.vercel.app/api";
 } else {
   // URL para ambiente de desenvolvimento (localhost)
   baseUrl = "http://localhost:3333";
